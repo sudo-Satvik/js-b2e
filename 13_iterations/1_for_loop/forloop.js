0854184469f0e57fe7e70ab3dd02ced37ce0347a
Satvik Sharma <37412303+sudo-Satvik@users.noreply.github.com>
2024-09-24 17:56:33 +0000

    for loop intro and if else with for loop finished

---
+++ 13_iterations/1_for_loop/forloop.js
@@ -0,0 +1,8 @@
+// For Loop
+
+// Used when we know about the number of iterations!
+
+for (let index = 1; index < 11; index++) {
+    console.log(index);
+}
+
---
+++ 13_iterations/1_for_loop/ifelse_forloop.js
@@ -0,0 +1,9 @@
+
+// if-else can also be used in the for loop
+
+for(let i = 1 ; i <= 5 ; i++){
+    console.log(i);
+    if(i == 2){
+        // console.log("best");
+    }
+}
