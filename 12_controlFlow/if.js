8ef734d16a94cec218e94d0a8c56655428a1dcd6
Satvik Sharma <37412303+sudo-Satvik@users.noreply.github.com>
2024-09-24 07:20:16 +0000

    If condition done

---
+++ 12_controlFlow/if.js
@@ -0,0 +1,19 @@
+// if statement
+let condition = true
+if(condition){
+    console.log("Code runs here");
+}
+
+if(!condition){
+    console.log("This code won't run");
+}
+
+// in condition we use relational operator
+/*
+    <, >, <=, >=, ==, !=, ===, !==
+*/
+
+if(2 !== '2'){
+    console.log(true)
+}
+    
