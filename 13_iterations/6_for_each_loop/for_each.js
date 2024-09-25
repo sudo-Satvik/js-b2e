a0ab0c7660d6e972ae14150a94e286c9df17095a
Satvik Sharma <37412303+sudo-Satvik@users.noreply.github.com>
2024-09-25 18:47:33 +0000

    for each finished

--- 13_iterations/6_for_each_loop/for_each.js
+++ 13_iterations/6_for_each_loop/for_each.js
@@ -27,3 +27,22 @@ coding.forEach((e, i, a) => {
     console.log(`Element ${e} from index ${i} which is in ${a} array`);
     
 })
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach((e) => {
+    console.log(e.languageFileName);
+})
