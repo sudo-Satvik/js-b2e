8205d44430209a5fed82340721765abf9a97cc4f
Satvik Sharma <37412303+sudo-Satvik@users.noreply.github.com>
2024-09-25 18:42:20 +0000

    for in finished

---
+++ 13_iterations/6_for_each_loop/for_each.js
@@ -0,0 +1,29 @@
+// Higher order function of array
+
+const coding = ["js", "ruby", "cpp"]
+
+// Call back function : name of the function shouldn't be there
+
+coding.forEach( function (e){
+    // console.log(e);
+} )
+
+// using arrow function
+
+// coding.forEach(() => {})
+
+// also we can do iteration like this
+
+function printMe(e){
+    // console.log(e);    
+}
+
+coding.forEach(printMe)         // we should have to give the function reference not to call the function
+
+// params: (element, index, array)
+
+
+coding.forEach((e, i, a) => {
+    console.log(`Element ${e} from index ${i} which is in ${a} array`);
+    
+})
